5eb159cb0fd2e43e73f8c17f190d5dd49180dc6b
eduardorittner <eduardorittnerc@gmail.com>
2025-10-20 10:08:06 -0300

    check isqrt

--- library/core/src/num/nonzero.rs
+++ library/core/src/num/nonzero.rs
@@ -1783,6 +1783,7 @@ macro_rules! nonzero_integer_signedness_dependent_methods {
         #[must_use = "this returns the result of the operation, \
                       without modifying the original"]
         #[inline]
+        #[ensures(|result| result.get() != 0)]
         pub const fn isqrt(self) -> Self {
             let result = self.get().isqrt();
 
